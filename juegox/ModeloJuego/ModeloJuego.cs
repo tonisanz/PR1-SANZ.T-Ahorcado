f64681768775d2c91ce2de3ecb7d3f46c30c46c9
tonisanz <tonisanzllacer@gmail.com>
2014-11-14 18:51:35 +0100

    CambiosMetodos

--- juegox/ModeloJuego/ModeloJuego.cs
+++ juegox/ModeloJuego/ModeloJuego.cs
@@ -3,14 +3,166 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SanzA.Programacion.Eval1.Juego.Logica
 {
     public static class ModeloJuego
     {
-        public static int SecretoDeLaVida()
+        
+        public static void AdivinarPalabra(ref int i, int tl, String palabra2, ref String palabra3, ref String le, ref String lp)
         {
-            return 42;
+            int intento = 0;
+
+
+
+            do
+            {
+                //String lp = "", le = "";
+                Boolean exito = false;
+                Console.Write("PULSA UNA TECLA");
+                Console.ReadLine();
+                lp = Console.ReadLine();
+                for (i = 0; i <= tl - 1; i++)
+                {
+
+                    le = palabra2.Substring(i, 1);
+                    if (le.Equals(lp))
+                    {
+
+                        palabra3 = palabra3.Remove(i, 1);
+                        palabra3 = palabra3.Insert(i, lp);
+                        exito = true;
+
+                    }
+
+                }
+
+                if (exito)
+                {
+                    Console.WriteLine("PALABRA = " + palabra3);
+                    Console.WriteLine();
+
+                }
+                else
+                {
+                    intento++;
+                    Console.WriteLine("La letra introducida no corresponde a la palabra, llevas " + intento + " intento");
+                    Console.WriteLine();
+
+                }
+
+                if (intento == 5)
+                {
+                    Console.WriteLine(" X");
+                    Console.WriteLine(" |");
+                    Console.WriteLine("\\  /");
+                    Console.WriteLine("/  \\");
+                    Console.ReadKey();
+                    Console.WriteLine("Has agotado todos tus intentos, ¡PIERDES!");
+                    Console.WriteLine();
+                    Console.WriteLine("LA PALABRA CORRECTA ERA {0}", palabra2);
+                }
+                if (intento == 0)
+                {
+                    Console.WriteLine(" O");
+                    Console.WriteLine(" -|-");
+                    Console.WriteLine("/  \\");
+
+
+                    Console.ReadLine();
+                }
+                if (intento == 1)
+                {
+                    Console.WriteLine(" -----");
+                    Console.WriteLine("    ||");
+                    Console.WriteLine("    |O");
+                    Console.WriteLine("   |/|\\");
+                    Console.WriteLine(" |");
+                    Console.WriteLine(" |");
+
+                    Console.ReadLine();
+                }
+                if (intento == 2)
+                {
+                    Console.WriteLine(" -----");
+                    Console.WriteLine(" ||");
+                    Console.WriteLine(" |O");
+
+                    Console.WriteLine(" |");
+                    Console.WriteLine(" |");
+
+                }
+                if (intento == 3)
+                {
+                    Console.WriteLine(" -----");
+                    Console.WriteLine(" ||");
+                    Console.WriteLine(" |O");
+                    Console.WriteLine("|/|\\");
+
+                    Console.WriteLine(" |");
+                    Console.WriteLine(" |");
+
+
+                }
+                if (intento == 4)
+                {
+                    Console.WriteLine(" O");
+                    Console.WriteLine(" |");
+                    Console.ReadLine();
+
+
+
+                }
+
+
+                if (palabra2 == palabra3)
+                {
+                    Console.WriteLine("ENHORABUENA! ¡GANASTE!");
+                    break;
+                }
+
+
+
+
+            }
+            while (intento < 5);
         }
+        public static void InterrogacionOEspacio(ref int i, int tl, String palabra2, ref String palabra3)
+        {
+            for (i = 1; i <= tl; i++)
+            {
+                if (palabra2.Substring(i, 0) != " ")
+                    palabra3 = palabra3 + "?";
+                else
+                    palabra3 = palabra3 + " ";
+
+            }
+
+
+            Console.WriteLine(palabra3);
+        }
+        public static void extraerPalabraAleatoria()
+        {
+            
+            
+                 
+            //Variable para la cantidad de palabras aleatorias a mostrar
+            int cantidad_de_palabras_aleatorias = 1;
+            //Creamos un objeto aleatorio
+            Random alea = new Random();
+            //String donde guardaremos la palabra aleatoria
+            String[]palabra = new String[alea.Next(1,100)];
+            
+            //Bucle principal hasta que se llegue a esa cantidad
+            for (int i = 0; i < cantidad_de_palabras_aleatorias; i++)
+            {
+                StreamReader objReader = new StreamReader("C:\\Users\\ToniSanz\\Source\\Repos\\ahorcado\\juegox\\palabras.txt");
+                string aleatorio = objReader.ReadLine();
+
+           
+            }
+
+}
     }
 }
--- juegox/cui/Program.cs
+++ juegox/cui/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SanzA.Programacion.Eval1.Juego.Logica;
+using System.IO;
 
 namespace SanzA.Programacion.Eval1.Juego.CUI
 {
@@ -11,9 +12,51 @@ namespace SanzA.Programacion.Eval1.Juego.CUI
     {
         static void Main(string[] args)
         {
-            int secreto = ModeloJuego.SecretoDeLaVida();
-            Console.WriteLine("El secreto de la vida es " + secreto);
-            Console.ReadLine();
+            
+            int cont = 100;
+            String[] palabra = new String[cont];
+            
+           
+            //Damos color a la consola y al texto
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            //Creamos un objeto aleatorio
+            
+            
+            //Creamos y Damos valores nulos a las variables de tipo int
+            int i = 0, numeroAleatorio = 0, longitudPalabra = 0;
+            //Inicializamos la variable numeroAleatorio entre 1 y 20
+            numeroAleatorio = alea.Next(1, cont);
+            //Creamos y damos valores vacios a las variables de tipo string
+            String palabra2 = "", palabra3 = "", le = "", lp = ""; ;
+
+            
+            //Inicializamos la variable string palabra2, asignándole el valor de la posicion del numero aleatorio
+            
+            palabra2 = objReader.ReadLine();
+            //Inicializamos la variable longitudPalabra, asignándole la longitud de la palabra escogida aleatoriamente
+            longitudPalabra = palabra2.Length;
+
+            //Escribimos el titulo del juego en consola
+            Console.WriteLine("...................");
+            Console.WriteLine();
+            Console.WriteLine("JUEGO DEL AHORCADO");
+            Console.WriteLine("...................");
+
+            Console.WriteLine("\nY tiene " + longitudPalabra + " letras");
+
+
+            //Llamamos al bucle for, InterrogacionOespacio para poner el simbolo ? o un espacio
+            ModeloJuego.InterrogacionOEspacio(ref i, longitudPalabra, palabra2, ref palabra3);
+
+
+            //Llamamos al método AdivinarPalabra para empezar a jugar
+            ModeloJuego.AdivinarPalabra(ref i, longitudPalabra, palabra2, ref palabra3, ref le, ref lp);
+
+            Console.ReadKey();
         }
+
     }
 }
